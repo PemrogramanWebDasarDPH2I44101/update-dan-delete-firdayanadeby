93507f986b78992087986434c030519b373895e4
firdayanadeby <43123859+firdayanadeby@users.noreply.github.com>
2018-10-05 11:57:58 +0100

    Update list.php

--- list.php
+++ list.php
@@ -1,30 +1,43 @@
 <?php
-require_once("db.php");git 
+include_once 'db.php';
 ?>
-<table border=1>
-    <thead>
-        <th>Nama</th>
-        <th>Nim</th>
-        <th>Tanggal Lahir</th>
-    </thead>
-    <tbody>
-<?php
-$sql    = "SELECT * FROM siswa";
-$result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row["nama"]. "</td>"; 
-        echo "<td>" . $row["nim"]. "</td>";
-        echo "<td>" . $row["tgl_lahir"]. "</td>";
-        echo "</tr>";
+<!DOCTYPE html>
+<html>
+<head>
+    <title>list.php</title>
+</head>
+<body>
+    <table border="1">
+        <thead>
+            <th>nama &nbsp &nbsp &nbsp &nbsp &nbsp</th>
+            <th>nim</th>
+            <th>tgl_lahir</th>
+            <th>action</th>
+        </thead>
+        <tbody>
+<?php
+$query = "SELECT * FROM siswa";
+$result = mysqli_query($conn,$query);
+if (mysqli_num_rows($result)>0) {
+    while ($row=mysqli_fetch_assoc($result)) {
+?>
+    <tr>
+        <td><?php echo $row['nama']; ?></td>
+        <td><?php echo $row['nim']; ?></td>
+        <td><?php echo $row['tgl_lahir']; ?></td>
+        <td><a href="edit.php?id=<?php echo $row['Id']; ?>">Edit</a> | <a href="delete.php?id=<?php echo $row['Id']; ?>" onclick="return confirm('apakah anda yakin?">Delete</a></td>
+    </tr>
+    <?php
+        }
+    }else {
+        echo "no result";
     }
-} else {
-    echo "0 results";
-}
-mysqli_close($conn);
-?> 
-    </tbody>
+?>
+</tbody>
 </table>
+<br>
+<a href="form.php">tambah data</a>
+
+</body>
+</html>
